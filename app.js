342edbd9175a3f6250bbef451b2c31180bf2c0c2
Sai Lokesh Chirumarri <76848659+sailokesh-hub@users.noreply.github.com>
2024-02-02 18:33:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mysql = require('mysql2/promise');
 const port = process.env.PORT || 3001;
 
 const connection = mysql.createConnection({

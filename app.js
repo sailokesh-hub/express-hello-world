6d7c1f4070843f08419af8413f57c72e9486d285
Sai Lokesh Chirumarri <76848659+sailokesh-hub@users.noreply.github.com>
2024-02-16 11:24:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ const fs = require("fs");
 const port = process.env.PORT || 3001;
 
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(fileUpload());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://24hr7comit.site');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');

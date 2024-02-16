8098abe3a494acb20d4815abc562a5fd40761dab
Sai Lokesh Chirumarri <76848659+sailokesh-hub@users.noreply.github.com>
2024-02-16 03:51:15 -0800

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(fileUpload());
+app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
